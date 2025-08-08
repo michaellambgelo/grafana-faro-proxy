f85cbf354f89268d4c74a38e28d0a6266b1aff26
Michael Lamb <michael@michaellamb.dev>
2025-08-08 11:06:24 -0500

    update worker.js

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With, x-faro-session-id',
   'Access-Control-Max-Age': '86400',
 };
 
